204feb5adf418c5fddd5e80b9e2cb850d18918a4
CarusoAugusto <103348874+ragnarcb@users.noreply.github.com>
2024-10-03 20:48:08 -0300

    .

---
+++ src/main/java/com/mim/myinvestimentmanager/controller/StockController.java
@@ -0,0 +1,60 @@
+package com.mim.myinvestimentmanager.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mim.myinvestimentmanager.model.Stock;
+import com.mim.myinvestimentmanager.service.interfaces.StockService;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+    
+    @Autowired
+    private StockService stockService;
+
+    @PostMapping
+    public ResponseEntity<Stock> save(@RequestBody Stock object){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.stockService.save(object));
+    }
+
+    @PostMapping("/savelist")
+    public ResponseEntity<List<Stock>> saveAll(@RequestBody List<Stock> stockList) {
+        return ResponseEntity.ok().body(this.stockService.saveAll(stockList));
+    }
+    
+
+    @GetMapping
+    public ResponseEntity<List<Stock>> listAll(){
+        return ResponseEntity.ok().body(this.stockService.list());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Stock> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(this.stockService.searchById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Stock> update(@RequestBody Stock object) {
+        return ResponseEntity.ok().body(this.stockService.update(object));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        this.stockService.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/mim/myinvestimentmanager/model/StockPurchase.java
@@ -0,0 +1,24 @@
+package com.mim.myinvestimentmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockPurchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String symbol;
+    private String companyName;
+    private Integer quantity;
+    private Double stockValue;
+}
